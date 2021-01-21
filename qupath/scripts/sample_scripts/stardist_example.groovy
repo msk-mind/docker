bbbce11032d3852040a2c09f3b92178a8b434f14
arfath pasha <arfathpasha@gmail.com>
2021-01-21 12:09:15 -0500

    minor update

--- qupath/scripts/sample_scripts/stardist_example.groovy
+++ qupath/scripts/sample_scripts/stardist_example.groovy
@@ -11,6 +11,20 @@ def stardist = StarDist2D.builder(pathModel)
 
 // Run detection for the selected objects
 def imageData = getCurrentImageData()
+
+def server = getCurrentImageData().getServer()
+// get dimensions of slide
+minX = 0
+minY = 0
+maxX = server.getWidth()
+maxY = server.getHeight()
+// create rectangle roi (over entire area of image) for detections to be run over
+def plane = ImagePlane.getPlane(0, 0)
+def roi = ROIs.createRectangleROI(minX, minY, maxX-minX, maxY-minY, plane)
+def annotationROI = PathObjects.createAnnotationObject(roi)
+addObject(annotationROI)
+selectAnnotations();
+
 def pathObjects = getSelectedObjects()
 if (pathObjects.isEmpty()) {
     Dialogs.showErrorMessage("StarDist", "Please select a parent object!")
